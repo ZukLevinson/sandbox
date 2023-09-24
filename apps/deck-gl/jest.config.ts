474b59c9493fd4a37525b493bb5676bc2c471807
Zuk Levinson <zuklevinson@gmail.com>
2023-09-24 21:01:53 +0300

    fix(snadbox): rename app

--- apps/deck-gl/jest.config.ts
+++ apps/deck-gl/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'sandbox',
+  displayName: 'deck-gl',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/sandbox',
+  coverageDirectory: '../../coverage/apps/deck-gl',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
